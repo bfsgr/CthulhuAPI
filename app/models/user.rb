e9e9558abe1b35a8d0803587959ceeb2dead4667
bfsgr <bfsgr.code@protonmail.com>
2021-02-14 15:32:01 -0300

    username and index

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
         :rememberable, :validatable, :jwt_authenticatable, jwt_revocation_strategy: Devise::JWT::RevocationStrategies::Null
+
+  validates :username, :presence => true, :uniqueness => true, :length => { :in => 4..255 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 2021_02_14_180125) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
