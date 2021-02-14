382a66566c036d3b0d3d237cd68bce9ed5c1df37
bfsgr <bfsgr.code@protonmail.com>
2021-02-14 15:40:11 -0300

    relations and protections

--- app/models/game_set.rb
+++ app/models/game_set.rb
@@ -2,4 +2,6 @@ class GameSet < ApplicationRecord
     has_many :skills, dependent: :destroy
     has_many :occupations, dependent: :destroy
     has_many :picks, dependent: :destroy
+
+    belongs_to :user, dependent: :destroy
 end
