110f1f96af729c788b6cf82261ac65ef112c773c
bfsgr <bfsgr.code@protonmail.com>
2021-02-13 23:25:23 -0300

    change database

--- config/application.rb
+++ config/application.rb
@@ -5,10 +5,10 @@ require "rails"
 require "active_model/railtie"
 require "active_job/railtie"
 require "active_record/railtie"
-require "active_storage/engine"
+# require "active_storage/engine"
 require "action_controller/railtie"
 require "action_mailer/railtie"
-require "action_mailbox/engine"
+# require "action_mailbox/engine"
 require "action_text/engine"
 require "action_view/railtie"
 require "action_cable/engine"
@@ -21,6 +21,12 @@ Bundler.require(*Rails.groups)
 
 module RCthulhuapi
   class Application < Rails::Application
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
