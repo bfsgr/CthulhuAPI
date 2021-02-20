be20f2d7743f6495f290dcf8e4d61eea71bb6c04
bfsgr <bfsgr.code@protonmail.com>
2021-02-20 13:54:23 -0300

    occupation tests and logic

--- app/models/occupation.rb
+++ app/models/occupation.rb
@@ -2,4 +2,31 @@ class Occupation < ApplicationRecord
     has_and_belongs_to_many :skills
     has_and_belongs_to_many :picks
     belongs_to :game_set, dependent: :destroy
+
+    validates :name, presence: true, :length => { in: 5..25 }
+    validates :calcType, presence: true
+    validates :creditLevel, presence: true, :length => { is: 2 }
+
+    validate :validate_creditLevel
+
+    def validate_creditLevel
+        if self.creditLevel.length == 2
+            
+            all_int = true
+            
+            self.creditLevel.each do |c|
+                if c.is_a?(Integer) 
+                    errors.add(:creditLevel, "#{c} should be within 1 and 99") unless c > 0 && c < 100
+                else
+                    all_int = false
+                end
+            end  
+
+            sorted = self.creditLevel.sort if all_int
+            
+            errors.add(:creditLevel, "credit level should be int") unless all_int
+            errors.add(:creditLevel, "credit level should sorted") unless sorted == self.creditLevel
+
+        end
+    end
 end
--- test/models/occupation_test.rb
+++ test/models/occupation_test.rb
@@ -7,4 +7,60 @@ class OccupationTest < ActiveSupport::TestCase
     assert oc.save
   end
   
+  test "occupation should not save without name" do
+    oc = Occupation.new(creditLevel: [30,70], calcType: "EDUx4", game_set_id: 1)
+    assert_not oc.save
+  end
+  
+  test "occupation should not save without credit level" do
+    oc = Occupation.new(name: "Advogado", calcType: "EDUx4", game_set_id: 1)
+    assert_not oc.save
+  end
+  
+  test "occupation should not save without calcType" do
+    oc = Occupation.new(name: "Advogado", creditLevel: [30,70], game_set_id: 1)
+    assert_not oc.save
+  end
+  
+  test "occupation should not save without a gameSet association" do
+    oc = Occupation.new(name: "Advogado", creditLevel: [30,70], calcType: "EDUx4")
+    assert_not oc.save
+  end
+  
+  test "occupation should not save with a name less than 5 characters" do
+    oc = Occupation.new(name: "", creditLevel: [30,70], calcType: "EDUx4", game_set_id: 1)
+    assert_not oc.save
+  end
+  
+  test "occupation should not save with a name more than 50 characters" do
+    oc = Occupation.new(
+      name: 'ANonfugaexpeditaautMole'\
+      'rumperspiciatisvoluptatemfug',
+      creditLevel: [30,70],
+      calcType: "EDUx4",
+      game_set_id: 1)
+      assert_not oc.save
+    end
+    
+  test "occupation should not save with creditLevel length different than 2" do
+    oc = Occupation.new(name: "Advogado", creditLevel: [30,70, 1], calcType: "EDUx4", game_set_id: 1)
+    assert_not oc.save
+  end
+  
+  test "occupation should not save with creditLevel with different types" do
+    oc = Occupation.new(name: "Advogado", creditLevel: [30, nil], calcType: "EDUx4", game_set_id: 1)
+    assert_not oc.save
+  end
+  
+  test "occupation should not save with creditLevel not sorted" do
+    oc = Occupation.new(name: "Advogado", creditLevel: [90, 10], calcType: "EDUx4", game_set_id: 1)
+    assert_not oc.save
+  end
+
+  test "occupation should not save with creditLevel outside of 1..99 range" do
+    oc = Occupation.new(name: "Advogado", creditLevel: [0, 102], calcType: "EDUx4", game_set_id: 1)
+    assert_not oc.save
+  end
+  
+
 end
