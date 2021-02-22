a9bbee1818129486ddf1c407ad3390cb2fc02795
bfsgr <bfsgr.code@protonmail.com>
2021-02-22 15:48:12 -0300

    inline fixture calls

--- test/models/pick_test.rb
+++ test/models/pick_test.rb
@@ -4,8 +4,7 @@ class PickTest < ActiveSupport::TestCase
 
   test "pick saves correctly" do
     pick = Pick.new(name: "One interpersonal", numberOfPicks: 1, pickAny: false, game_set: game_sets(:default))
-    pick.skills << skills(:briga)
-    pick.skills << skills(:labia)
+    pick.skills << skills(:briga, :labia)
     assert pick.save
   end
 
@@ -56,8 +55,7 @@ class PickTest < ActiveSupport::TestCase
 
   test "pick should not save if one of the skills is not from the same gameset" do
     pick = Pick.new(name: "One interpersonal", numberOfPicks: 1, pickAny: false, game_set: game_sets(:default))
-    pick.skills << skills(:briga)
-    pick.skills << skills(:computadores)
+    pick.skills << skills(:briga, :computadores)
     assert_not pick.save
   end
 
@@ -89,18 +87,12 @@ class PickTest < ActiveSupport::TestCase
   
   test "two picks should save with the same name in different game sets" do
     pick1 = Pick.new(name: "2 interpessoais", numberOfPicks: 2, pickAny: false,  game_set: game_sets(:default))
-    pick1.skills << skills(:labia)
-    pick1.skills << skills(:persuasao)
-    pick1.skills << skills(:charme)
-    pick1.skills << skills(:intimidacao)
+    pick1.skills << skills(:labia, :persuasao, :charme, :intimidacao)
     
     assert pick1.save
     
     pick2 = Pick.new(name: "2 interpessoais", numberOfPicks: 2, pickAny: false,  game_set: game_sets(:modern))
-    pick2.skills << skills(:labia_m)
-    pick2.skills << skills(:persuasao_m)
-    pick2.skills << skills(:charme_m)
-    pick2.skills << skills(:intimidacao_m)
+    pick2.skills << skills(:labia_m, :persuasao_m, :charme_m, :intimidacao_m)
 
     assert pick2.save
   end
