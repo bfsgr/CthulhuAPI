a658645442cf88f28908d9716778db81bb670fa4
bfsgr <bfsgr.code@protonmail.com>
2021-03-04 20:38:52 -0300

    scope routes + don't return body in user creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users, defaults: { format: :json }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :occupations
-  resources :skills
-  resources :picks
+  scope :api do
+    devise_for :users, path: 'user', defaults: { format: :json }, controllers: {
+      registrations: 'users/registrations'
+    }
+  
+    resources :occupations
+    resources :skills
+    resources :picks
+  
+  end
+
 end
