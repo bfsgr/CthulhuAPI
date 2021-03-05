67cfba4573c3221a190b97ce30fb3990508107b3
bfsgr <bfsgr.code@protonmail.com>
2021-03-05 20:04:27 -0300

    user tests and plural

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,8 @@ class User < ApplicationRecord
 	devise :database_authenticatable, :registerable,
 								:rememberable, :validatable, :jwt_authenticatable, jwt_revocation_strategy: self
 
-	validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { in: 4..255 }
+	validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { in: 4..25 }
 	validates_format_of :username, with: /^[a-zA-Z0-9_.]*$/, multiline: true
 
-	has_many :game_set
-
+	has_many :game_sets
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,5 +1,29 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-	# managed by devise, so no test is needed
+	test 'user saves correctly' do
+		user = User.new(username: 'John', email: 'john@example.com', password: 'qwerty1234')
+		user.game_sets << GameSet.create(name: 'Default')
+		assert user.save
+	end
+
+	test 'user should not save without username' do
+		user = User.new(email: 'john@example.com', password: 'qwerty1234')
+		assert_not user.save
+	end
+
+	test 'user should not save with username less than 4 characters' do
+		user = User.new(username: 'ana', email: 'john@example.com', password: 'qwerty1234')
+		assert_not user.save
+	end
+
+	test 'user should not save with username more than 25 characters' do
+		user = User.new(username: 'aaaaaaaaaaaaaaaaaaaaaaaaaa', email: 'john@example.com', password: 'qwerty1234')
+		assert_not user.save
+	end
+
+	test 'user should not save with username with @' do
+		user = User.new(username: 'John@home', email: 'john@example.com', password: 'qwerty1234')
+		assert_not user.save
+	end
 end
