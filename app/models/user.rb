cb234f75e9f880c9ff63677cd3889101f9294a95
bfsgr <bfsgr.code@protonmail.com>
2021-03-05 15:37:28 -0300

    change JWT Recovation Strategy

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
+	include Devise::JWT::RevocationStrategies::JTIMatcher
 	# Include default devise modules. Others available are:
 	# :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 	devise :database_authenticatable, :registerable,
-								:rememberable, :validatable, :jwt_authenticatable, jwt_revocation_strategy: Devise::JWT::RevocationStrategies::Null
+								:rememberable, :validatable, :jwt_authenticatable, jwt_revocation_strategy: self
 
 	validates :username, presence: true, uniqueness: true, length: { in: 4..255 }
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,77 +10,81 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_210_305_175_328) do
-	# These are extensions that must be enabled in order to support this database
-	enable_extension 'plpgsql'
+ActiveRecord::Schema.define(version: 2021_03_05_182324) do
 
-	create_table 'game_sets', force: :cascade do |t|
-		t.string 'name'
-		t.datetime 'created_at', precision: 6, null: false
-		t.datetime 'updated_at', precision: 6, null: false
-		t.bigint 'user_id'
-		t.index ['user_id'], name: 'index_game_sets_on_user_id'
-	end
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
-	create_table 'occupations', force: :cascade do |t|
-		t.string 'name'
-		t.integer 'min_credit'
-		t.integer 'max_credit'
-		t.string 'calc_type'
-		t.datetime 'created_at', precision: 6, null: false
-		t.datetime 'updated_at', precision: 6, null: false
-		t.bigint 'game_set_id'
-		t.index ['game_set_id'], name: 'index_occupations_on_game_set_id'
-		t.index %w[name game_set_id], name: 'index_occupations_on_name_and_game_set_id', unique: true
-	end
+  create_table "game_sets", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_game_sets_on_user_id"
+  end
 
-	create_table 'occupations_picks', id: false, force: :cascade do |t|
-		t.bigint 'occupation_id', null: false
-		t.bigint 'pick_id', null: false
-	end
+  create_table "occupations", force: :cascade do |t|
+    t.string "name"
+    t.integer "min_credit"
+    t.integer "max_credit"
+    t.string "calc_type"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "game_set_id"
+    t.index ["game_set_id"], name: "index_occupations_on_game_set_id"
+    t.index ["name", "game_set_id"], name: "index_occupations_on_name_and_game_set_id", unique: true
+  end
 
-	create_table 'occupations_skills', id: false, force: :cascade do |t|
-		t.bigint 'occupation_id', null: false
-		t.bigint 'skill_id', null: false
-	end
+  create_table "occupations_picks", id: false, force: :cascade do |t|
+    t.bigint "occupation_id", null: false
+    t.bigint "pick_id", null: false
+  end
 
-	create_table 'picks', force: :cascade do |t|
-		t.integer 'number_of_picks'
-		t.boolean 'pick_any'
-		t.datetime 'created_at', precision: 6, null: false
-		t.datetime 'updated_at', precision: 6, null: false
-		t.bigint 'game_set_id'
-		t.string 'name'
-		t.index ['game_set_id'], name: 'index_picks_on_game_set_id'
-		t.index %w[name game_set_id], name: 'index_picks_on_name_and_game_set_id', unique: true
-	end
+  create_table "occupations_skills", id: false, force: :cascade do |t|
+    t.bigint "occupation_id", null: false
+    t.bigint "skill_id", null: false
+  end
 
-	create_table 'picks_skills', id: false, force: :cascade do |t|
-		t.bigint 'skill_id', null: false
-		t.bigint 'pick_id', null: false
-	end
+  create_table "picks", force: :cascade do |t|
+    t.integer "number_of_picks"
+    t.boolean "pick_any"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "game_set_id"
+    t.string "name"
+    t.index ["game_set_id"], name: "index_picks_on_game_set_id"
+    t.index ["name", "game_set_id"], name: "index_picks_on_name_and_game_set_id", unique: true
+  end
 
-	create_table 'skills', force: :cascade do |t|
-		t.string 'name'
-		t.integer 'base_value'
-		t.datetime 'created_at', precision: 6, null: false
-		t.datetime 'updated_at', precision: 6, null: false
-		t.bigint 'game_set_id'
-		t.index ['game_set_id'], name: 'index_skills_on_game_set_id'
-		t.index %w[name game_set_id], name: 'index_skills_on_name_and_game_set_id', unique: true
-	end
+  create_table "picks_skills", id: false, force: :cascade do |t|
+    t.bigint "skill_id", null: false
+    t.bigint "pick_id", null: false
+  end
 
-	create_table 'users', force: :cascade do |t|
-		t.string 'username', default: '', null: false
-		t.string 'email', default: '', null: false
-		t.string 'encrypted_password', default: '', null: false
-		t.string 'reset_password_token'
-		t.datetime 'reset_password_sent_at'
-		t.datetime 'remember_created_at'
-		t.datetime 'created_at', precision: 6, null: false
-		t.datetime 'updated_at', precision: 6, null: false
-		t.index ['email'], name: 'index_users_on_email', unique: true
-		t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
-		t.index ['username'], name: 'index_users_on_username', unique: true
-	end
+  create_table "skills", force: :cascade do |t|
+    t.string "name"
+    t.integer "base_value"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "game_set_id"
+    t.index ["game_set_id"], name: "index_skills_on_game_set_id"
+    t.index ["name", "game_set_id"], name: "index_skills_on_name_and_game_set_id", unique: true
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username", default: "", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "jti", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["jti"], name: "index_users_on_jti", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
+  end
+
 end
