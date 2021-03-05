76b0350dd1a97d90f810f5f6391cad15df1f62e8
bfsgr <bfsgr.code@protonmail.com>
2021-03-05 03:05:02 -0300

    better complexity

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -15,21 +15,19 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
 		resource.save
 		yield resource if block_given?
-		if resource.persisted?
-			if resource.active_for_authentication?
-				set_flash_message! :notice, :signed_up
-				sign_up(resource_name, resource)
-				response.headers['Location'] = request.protocol + request.host + '/home'
-				head :created
-			else
-				set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
-				expire_data_after_sign_in!
-				respond_with resource, location: after_inactive_sign_up_path_for(resource)
-			end
-		else
+		if not resource.persisted?
 			clean_up_passwords resource
 			set_minimum_password_length
 			respond_with resource
+		elsif resource.active_for_authentication?
+			set_flash_message! :notice, :signed_up
+			sign_up(resource_name, resource)
+			response.headers['Location'] = "#{request.protocol}#{request.host}/home"
+			head :created
+		else
+			set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+			expire_data_after_sign_in!
+			respond_with resource, location: after_inactive_sign_up_path_for(resource)
 		end
 	end
 
