be46273fd28a93a24fe799812f9ecfd1f463f9e8
bfsgr <bfsgr.code@protonmail.com>
2021-03-05 20:04:13 -0300

    make simplecov work with spring

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,6 @@
-require 'simplecov'
-SimpleCov.start 'rails'
-
 ENV['RAILS_ENV'] ||= 'test'
+SimpleCov.start 'rails'
+Rails.application.eager_load!
 require_relative '../config/environment'
 require 'rails/test_help'
 
