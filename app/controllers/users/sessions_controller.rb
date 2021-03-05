7e74916c312ea140be144ff9140ccaf858d0314b
bfsgr <bfsgr.code@protonmail.com>
2021-03-05 16:36:45 -0300

    no body in login success

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -9,9 +9,14 @@ class Users::SessionsController < Devise::SessionsController
 	# end
 
 	# POST /resource/sign_in
-	# def create
-	# 	super
-	# end
+	def create
+		self.resource = warden.authenticate!(auth_options)
+		set_flash_message!(:notice, :signed_in)
+		sign_in(resource_name, resource)
+		yield resource if block_given?
+		response.headers['Location'] = "#{request.protocol}#{request.host}/home"
+		head :created
+	end
 
 	# DELETE /resource/sign_out
 	# def destroy
