ee7d0d908e95655fcc096761b31b0af0e18f2397
bfsgr <bfsgr.code@protonmail.com>
2021-03-04 23:28:04 -0300

    fix tests

--- test/models/occupation_test.rb
+++ test/models/occupation_test.rb
@@ -2,14 +2,14 @@ require 'test_helper'
 
 class OccupationTest < ActiveSupport::TestCase
   test "occupation saves correctly" do
-    oc = Occupation.new(name: "Advogado", creditLevel: [30,70], calcType: "EDUx4",  game_set: game_sets(:default))
+    oc = Occupation.new(name: "Advogado", min_credit: 30, max_credit: 70, calcType: "EDUx4",  game_set: game_sets(:default))
     oc.picks << picks(:interpessoal)
     oc.skills << skills(:briga)
     assert oc.save
   end
   
   test "occupation should not save without name" do
-    oc = Occupation.new(creditLevel: [30,70], calcType: "EDUx4",  game_set: game_sets(:default))
+    oc = Occupation.new(min_credit: 30, max_credit: 70, calcType: "EDUx4",  game_set: game_sets(:default))
     assert_not oc.save
   end
   
@@ -19,17 +19,17 @@ class OccupationTest < ActiveSupport::TestCase
   end
   
   test "occupation should not save without calcType" do
-    oc = Occupation.new(name: "Advogado", creditLevel: [30,70],  game_set: game_sets(:default))
+    oc = Occupation.new(name: "Advogado", min_credit: 30, max_credit: 70,  game_set: game_sets(:default))
     assert_not oc.save
   end
   
   test "occupation should not save without a gameSet association" do
-    oc = Occupation.new(name: "Advogado", creditLevel: [30,70], calcType: "EDUx4")
+    oc = Occupation.new(name: "Advogado", min_credit: 30, max_credit: 70, calcType: "EDUx4")
     assert_not oc.save
   end
   
   test "occupation should not save with a name less than 5 characters" do
-    oc = Occupation.new(name: "", creditLevel: [30,70], calcType: "EDUx4",  game_set: game_sets(:default))
+    oc = Occupation.new(name: "", min_credit: 30, max_credit: 70, calcType: "EDUx4",  game_set: game_sets(:default))
     assert_not oc.save
   end
   
@@ -37,60 +37,46 @@ class OccupationTest < ActiveSupport::TestCase
     oc = Occupation.new(
       name: 'ANonfugaexpeditaautMole'\
       'rumperspiciatisvoluptatemfug',
-      creditLevel: [30,70],
+      min_credit: 30,
+      max_credit: 70,
       calcType: "EDUx4",
        game_set: game_sets(:default))
       assert_not oc.save
     end
-    
-  test "occupation should not save with creditLevel length different than 2" do
-    oc = Occupation.new(name: "Advogado", creditLevel: [30,70, 1], calcType: "EDUx4",  game_set: game_sets(:default))
-    assert_not oc.save
-  end
-  
-  test "occupation should not save with creditLevel with different types" do
-    oc = Occupation.new(name: "Advogado", creditLevel: [30, nil], calcType: "EDUx4",  game_set: game_sets(:default))
-    assert_not oc.save
-  end
-  
-  test "occupation should not save with creditLevel not sorted" do
-    oc = Occupation.new(name: "Advogado", creditLevel: [90, 10], calcType: "EDUx4",  game_set: game_sets(:default))
-    assert_not oc.save
-  end
 
   test "occupation should not save with creditLevel outside of 1..99 range" do
-    oc = Occupation.new(name: "Advogado", creditLevel: [0, 102], calcType: "EDUx4",  game_set: game_sets(:default))
+    oc = Occupation.new(name: "Advogado",  min_credit: 0, max_credit: 102, calcType: "EDUx4",  game_set: game_sets(:default))
     assert_not oc.save
   end
   
   test "occupation should not save with skill from different game set" do
-    oc = Occupation.new(name: "Advogado", creditLevel: [30, 70], calcType: "EDUx4",  game_set: game_sets(:default))
+    oc = Occupation.new(name: "Advogado", min_credit: 30, max_credit: 70, calcType: "EDUx4",  game_set: game_sets(:default))
     oc.skills << skills(:computadores)
     assert_not oc.save
   end
 
   test "occupation should not save with picks from different game set" do
-    oc = Occupation.new(name: "Advogado", creditLevel: [30, 70], calcType: "EDUx4", game_set: game_sets(:default))  
+    oc = Occupation.new(name: "Advogado", min_credit: 30, max_credit: 70, calcType: "EDUx4", game_set: game_sets(:default))  
     oc.picks << picks(:interpessoal_modern)
     assert_not oc.save
   end
   
   test "two occupations should not save with the same name in the same game set" do
-    oc1 = Occupation.new(name: "Advogado", creditLevel: [30, 70], calcType: "EDUx4", game_set: game_sets(:default))  
+    oc1 = Occupation.new(name: "Advogado", min_credit: 30, max_credit: 70, calcType: "EDUx4", game_set: game_sets(:default))  
     
     assert oc1.save
     
-    oc2 = Occupation.new(name: "Advogado", creditLevel: [30, 70], calcType: "EDUx4", game_set: game_sets(:default))  
+    oc2 = Occupation.new(name: "Advogado", min_credit: 30, max_credit: 70, calcType: "EDUx4", game_set: game_sets(:default))  
     
     assert_not oc2.save
   end
 
   test "two occupations should save with the same name in different game sets" do
-    oc1 = Occupation.new(name: "Advogado", creditLevel: [30, 70], calcType: "EDUx4", game_set: game_sets(:default))  
+    oc1 = Occupation.new(name: "Advogado", min_credit: 30, max_credit: 70, calcType: "EDUx4", game_set: game_sets(:default))  
     
     assert oc1.save
     
-    oc2 = Occupation.new(name: "Advogado", creditLevel: [30, 70], calcType: "EDUx4", game_set: game_sets(:modern))  
+    oc2 = Occupation.new(name: "Advogado", min_credit: 30, max_credit: 70, calcType: "EDUx4", game_set: game_sets(:modern))  
     
     assert oc2.save
   end
