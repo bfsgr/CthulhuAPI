fe6dfbd0b5701217753beaf2602d610848c20f10
bfsgr <bfsgr.code@protonmail.com>
2021-03-07 17:15:44 -0300

    more tests

--- test/controllers/users/sessions_controller_test.rb
+++ test/controllers/users/sessions_controller_test.rb
@@ -10,4 +10,14 @@ class UserSessionsControllerTest < ActionDispatch::IntegrationTest
     }
     assert_response :created
   end
+
+  test 'user does not sign in due to invalid password' do
+    post '/api/user/sign_in', params: {
+      user: {
+        username: users(:first).username,
+        password: '12345678'
+      }
+    }
+    assert_response :unauthorized
+  end
 end
