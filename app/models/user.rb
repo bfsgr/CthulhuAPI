51d866016b856f188331caf18de677a8f155437d
bfsgr <bfsgr.code@protonmail.com>
2021-03-07 18:41:48 -0300

    disable rememberable

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :rememberable, :validatable, :jwt_authenticatable, jwt_revocation_strategy: self
+         :validatable, :jwt_authenticatable, jwt_revocation_strategy: self
 
   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { in: 4..25 }
   validates_format_of :username, with: /^[a-zA-Z0-9_.]*$/, multiline: true
