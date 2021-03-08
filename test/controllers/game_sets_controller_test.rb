e909df2da2d56d70be79d556e80df8badf55c117
bfsgr <bfsgr.code@protonmail.com>
2021-03-08 15:26:48 -0300

    expect id in body + use id to get details

--- test/controllers/game_sets_controller_test.rb
+++ test/controllers/game_sets_controller_test.rb
@@ -13,9 +13,22 @@ class GameSetsControllerTest < ActionDispatch::IntegrationTest
     expected = []
 
     gs.each do |gameset|
-      expected.push({ name: gameset.name })
+      expected.push({ id: gameset.id, name: gameset.name })
     end
 
     assert_equal expected.as_json, actual
+  end
+
+  test 'show details of any given game set' do
+    sign_in users(:first)
+    get '/api/game_sets', xhr: true
+    assert_response :ok
+
+    data = JSON.parse(@response.body)
+    identifier = data.first['id']
+
+    sign_in users(:first)
+    get "/api/game_sets/#{identifier}", xhr: true
+    assert_response :ok
   end
 end
