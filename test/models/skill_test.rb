8c09cef23a9995d3a57251a2714624ebfe10031a
bfsgr <bfsgr.code@protonmail.com>
2021-03-08 19:20:48 -0300

    refactor skill and user tests

--- test/models/skill_test.rb
+++ test/models/skill_test.rb
@@ -1,71 +1,66 @@
 require 'test_helper'
 
 class SkillTest < ActiveSupport::TestCase
+  def setup
+    @skill = Skill.new(name: 'Medicina', base_value: 5, game_set: game_sets(:default))
+  end
+
   test 'skill saves correctly' do
-    skill = Skill.new(name: 'Medicina', base_value: 5, game_set: game_sets(:default))
-    assert skill.save
+    assert @skill.save
   end
 
   test 'skill should not save without a name' do
-    skill = Skill.new(base_value: 10, game_set: game_sets(:default))
-    assert_not skill.save
+    @skill.name = nil
+    assert_not @skill.save
   end
 
   test 'skill should not save without a game set association' do
-    skill = Skill.new(name: 'Briga', base_value: 10)
-    assert_not skill.save
+    @skill.game_set = nil
+    assert_not @skill.save
   end
 
   test 'skill should not save with name less than 4 characters' do
-    skill = Skill.new(name: 'ana', base_value: 10, game_set: game_sets(:default))
-    assert_not skill.save
+    @skill.name = 'ana'
+    assert_not @skill.save
   end
 
   test 'skill should not save with name more than 50 characters' do
-    skill = Skill.new(
-      name: 'ANonfugaexpeditaautMole'\
-             'rumperspiciatisvoluptatemfug',
-      base_value: 10, game_set: game_sets(:default)
-    )
-    assert_not skill.save
+    @skill.name = 'ANonfugaexpeditaautMolerumperspiciatisvoluptatemfug'
+    assert_not @skill.save
   end
 
   test 'skill should not save without a base value' do
-    skill = Skill.new(name: '', game_set: game_sets(:default))
-    assert_not skill.save
+    @skill.base_value = nil
+    assert_not @skill.save
   end
 
   test 'skill should not save with a negative base value' do
-    skill = Skill.new(name: 'Medicina', base_value: -1, game_set: game_sets(:default))
-    assert_not skill.save
+    @skill.base_value = -1
+    assert_not @skill.save
   end
 
   test 'skill should not save with a base value greater than 99' do
-    skill = Skill.new(name: 'Medicina', base_value: 100, game_set: game_sets(:default))
-    assert_not skill.save
+    @skill.base_value = 100
+    assert_not @skill.save
   end
 
   test 'skill should not save with a base value that is not an integer' do
-    skill = Skill.new(name: 'Medicina', base_value: 4.12, game_set: game_sets(:default))
-    assert_not skill.save
+    @skill.base_value = 4.14
+    assert_not @skill.save
   end
 
   test 'two skills should not save with the same name in the same game set' do
-    skill1 = Skill.new(name: 'Medicina', base_value: 5, game_set: game_sets(:default))
+    assert @skill.save
 
-    assert skill1.save
-
-    skill2 = Skill.new(name: 'Medicina', base_value: 5, game_set: game_sets(:default))
+    skill2 = Skill.new(name: @skill.name, base_value: 5, game_set: game_sets(:default))
 
     assert_not skill2.save
   end
 
   test 'two skills should save with the same name in different game sets' do
-    skill1 = Skill.new(name: 'Medicina', base_value: 5, game_set: game_sets(:default))
+    assert @skill.save
 
-    assert skill1.save
-
-    skill2 = Skill.new(name: 'Medicina', base_value: 5, game_set: game_sets(:modern))
+    skill2 = Skill.new(name: @skill.name, base_value: 5, game_set: game_sets(:modern))
 
     assert skill2.save
   end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,29 +1,32 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  def setup
+    @user = User.new(username: 'John', email: 'john@example.com', password: 'qwerty1234')
+  end
+
   test 'user saves correctly' do
-    user = User.new(username: 'John', email: 'john@example.com', password: 'qwerty1234')
-    user.game_sets << GameSet.create(name: 'Default')
-    assert user.save
+    @user.game_sets << GameSet.create(name: 'Default')
+    assert @user.save
   end
 
   test 'user should not save without username' do
-    user = User.new(email: 'john@example.com', password: 'qwerty1234')
-    assert_not user.save
+    @user.username = nil
+    assert_not @user.save
   end
 
   test 'user should not save with username less than 4 characters' do
-    user = User.new(username: 'ana', email: 'john@example.com', password: 'qwerty1234')
-    assert_not user.save
+    @user.username = 'ana'
+    assert_not @user.save
   end
 
   test 'user should not save with username more than 25 characters' do
-    user = User.new(username: 'aaaaaaaaaaaaaaaaaaaaaaaaaa', email: 'john@example.com', password: 'qwerty1234')
-    assert_not user.save
+    @user.username = 'aaaaaaaaaaaaaaaaaaaaaaaaaa'
+    assert_not @user.save
   end
 
   test 'user should not save with username with @' do
-    user = User.new(username: 'John@home', email: 'john@example.com', password: 'qwerty1234')
-    assert_not user.save
+    @user.username = 'John@home'
+    assert_not @user.save
   end
 end
