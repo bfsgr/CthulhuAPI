6c72cbda649d236a1cc020656aab058b226f0320
bfsgr <bfsgr.code@protonmail.com>
2021-03-09 17:54:04 -0300

    refactor game set tests

--- test/models/game_set_test.rb
+++ test/models/game_set_test.rb
@@ -1,23 +1,27 @@
 require 'test_helper'
 
 class GameSetTest < ActiveSupport::TestCase
+  def setup
+    @gs = GameSet.new(name: 'Default', user: users(:first))
+  end
+
   test 'game set should not save without name' do
-    gs = GameSet.new(user: users(:first))
-    assert_not gs.save
+    @gs.name = nil
+    assert_not @gs.save
   end
 
   test 'game set should not save without user association' do
-    gs = GameSet.new(name: 'Default')
-    assert_not gs.save
+    @gs.user = nil
+    assert_not @gs.save
   end
 
   test 'game set should not save with name less than 5 characters' do
-    gs = GameSet.new(name: 'H', user: users(:first))
-    assert_not gs.save
+    @gs.name = 'ana'
+    assert_not @gs.save
   end
 
   test 'game set should not save with name more than 25 characters' do
-    gs = GameSet.new(name: 'ANonfugaexpeditaautMoleaef', user: users(:first))
-    assert_not gs.save
+    @gs.name = 'ANonfugaexpeditaautMoleaef'
+    assert_not @gs.save
   end
 end
