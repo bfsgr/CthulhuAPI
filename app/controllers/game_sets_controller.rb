21e143006fe479c1edc14c17ea5067a6f3728085
bfsgr <bfsgr.code@protonmail.com>
2021-03-09 18:49:14 -0300

    game set creation code

--- app/controllers/game_sets_controller.rb
+++ app/controllers/game_sets_controller.rb
@@ -11,9 +11,25 @@ class GameSetsController < ApplicationController
     render 'show'
   end
 
-  def create; end
+  def create
+    @game_set = GameSet.new(permitted_params)
+    @game_set.user = current_user
+
+    if @game_set.save
+      response.headers['Location'] = request.original_url + game_set_path(@game_set)
+      head :created
+    else
+      render 'create', status: :unprocessable_entity
+    end
+  end
 
   def update; end
 
   def destroy; end
+
+  private
+
+  def permitted_params
+    params.require(:game_set).permit(:name) if params[:game_set] && !params[:game_set].empty?
+  end
 end
---
+++ app/views/game_sets/create.json.jbuilder
@@ -0,0 +1,1 @@
+json.errors @game_set.errors
