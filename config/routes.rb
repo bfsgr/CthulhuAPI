9d95eabe07a5067199396c6ca8a8144530cf08e5
bfsgr <bfsgr.code@protonmail.com>
2021-03-11 16:12:31 -0300

    use standard responses

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   scope :api do
-    devise_for :users, path: 'user', defaults: { format: :json }, controllers: {
-      registrations: 'users/registrations',
-      sessions: 'users/sessions'
-    }
+    devise_for :users, path: 'user', defaults: { format: :json }
 
     resources :occupations
     resources :skills
