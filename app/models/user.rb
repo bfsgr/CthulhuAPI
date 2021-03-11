388b343aa3cca3be2058a9bffb9dbe45221195ee
bfsgr <bfsgr.code@protonmail.com>
2021-03-11 16:33:35 -0300

    add serializer to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   include Devise::JWT::RevocationStrategies::JTIMatcher
+  include ActiveModel::Serialization
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -9,4 +10,12 @@ class User < ApplicationRecord
   validates_format_of :username, with: /^[a-zA-Z0-9_.]*$/, multiline: true
 
   has_many :game_sets
+
+  def attributes
+    { id: nil,
+      username: nil,
+      email: nil,
+      created_at: nil,
+      updated_at: nil }
+  end
 end
