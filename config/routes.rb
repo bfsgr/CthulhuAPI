c95c19078c91cf2d31efdaccbcddca3a0cc19b8b
bfsgr <bfsgr.code@protonmail.com>
2021-03-22 01:49:39 -0300

    override repond_to_on_destroy to avoid error 500

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   scope :api do
-    devise_for :users, path: 'user', defaults: { format: :json }
+    devise_for :users, path: 'user', defaults: { format: :json }, controllers: {
+      sessions: 'users/sessions'
+    }
 
     resources :occupations
     resources :skills
