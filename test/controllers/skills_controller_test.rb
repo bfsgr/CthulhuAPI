6cb3e122d8eb8df36d69fb842e6e20dff16903fe
bfsgr <bfsgr.code@protonmail.com>
2021-04-23 16:34:29 -0300

    Test index skills

--- test/controllers/skills_controller_test.rb
+++ test/controllers/skills_controller_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class SkillsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::Test::IntegrationHelpers
+
+  test 'list skills correctly' do
+    sign_in users(:first)
+
+    get game_set_skills_path(game_sets(:default)), xhr: true
+
+    assert_response :ok
+
+    actual = JSON.parse(@response.body)
+
+    assert_equal game_sets(:default).skills.as_json, actual
+  end
 end
