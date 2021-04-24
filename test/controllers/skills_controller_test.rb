8b091dc805eb4475927876b3c18c36b64e889057
bfsgr <bfsgr.code@protonmail.com>
2021-04-23 21:43:25 -0300

    tests for skill#create

--- test/controllers/skills_controller_test.rb
+++ test/controllers/skills_controller_test.rb
@@ -14,4 +14,51 @@ class SkillsControllerTest < ActionDispatch::IntegrationTest
 
     assert_equal game_sets(:default).skills.as_json, actual
   end
+
+  test 'list skills should return not found' do
+    sign_in users(:first)
+
+    assert_raise ActiveRecord::RecordNotFound do
+      get '/api/game_sets/1/skills', xhr: true
+    end
+  end
+
+  test 'create skill correctly' do
+    sign_in users(:first)
+
+    post game_set_skills_path(game_sets(:default)), xhr: true, params: {
+      skill: {
+        name: 'Fight',
+        base_value: 10
+      }
+    }
+
+    assert_response :created
+  end
+
+  test 'create skill fails due to validation errors' do
+    sign_in users(:first)
+
+    post game_set_skills_path(game_sets(:default)), xhr: true, params: {
+      skill: {
+        name: '',
+        base_value: 10
+      }
+    }
+
+    assert_response :unprocessable_entity
+  end
+
+  test 'create skill fails due to not found game set' do
+    sign_in users(:first)
+
+    assert_raise ActiveRecord::RecordNotFound do
+      post '/api/game_sets/1/skills', xhr: true, params: {
+        skill: {
+          name: 'Fight',
+          base_value: 10
+        }
+      }
+    end
+  end
 end
